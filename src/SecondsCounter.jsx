042475d038a39063dca7511cc7911e15996b45a2
CarVill30 <carlosrogervillarreal3@gmail.com>
2025-03-31 15:12:18 +0000

    cambios en el Home

---
+++ src/SecondsCounter.jsx
@@ -0,0 +1,3 @@
+import React, { useState, useEffect} from "react";
+import ReactDOM from "react-dom/client";
+import "https://cdnjs.cloudflare"
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+// Import React into the bundle
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// Include bootstrap npm library into the bundle
+import 'bootstrap';
+import './icons.js';
+import SimpleCounter from "./component/simpleCounter.jsx"
 
-//Bootstrap
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap"
+// Include your index.scss file into the bundle
+import '../styles/index.scss';
 
-// index.css'
-import '../styles/index.css'
 
-// components
-import Home from './components/Home';
+let counter = 0;
+const root = ReactDOM.createRoot(document.getElementById('app'));
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
-)
+setInterval(() => {
+    const four = Math.floor(counter / 1000);
+    const three = Math.floor(counter / 100);
+    const two = Math.floor(counter / 10);
+    const one = Math.floor(counter / 1);
+    counter++;
+    
+    root.render(
+        <SimpleCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} />
+    );
+}, 1000);
